31a9af8334c49e68365f8604fac5496686c0d24b
jovemTranquilao <jovem@hocem.com>
2023-08-20 16:37:02 -0300

    Projeto inicial

---
+++ ServidorPublicoAPI/src/main/java/br/gov/jovemtranquilao/ServidorPublicoApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.jovemtranquilao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorPublicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorPublicoApiApplication.class, args);
+	}
+
+}
---
+++ ServidorPublicoAPI/src/test/java/br/gov/jovemtranquilao/ServidorPublicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.jovemtranquilao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorPublicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
