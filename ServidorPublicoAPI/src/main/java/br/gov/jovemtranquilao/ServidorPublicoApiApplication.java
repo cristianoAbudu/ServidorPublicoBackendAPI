9733ceec5c76fecaba5164b8a1538536baf3ea3a
jovemTranquilao <jovem@hocem.com>
2023-08-20 17:25:51 -0300

    Adicionado Spring web

--- ServidorPublicoAPI/src/main/java/br/gov/jovemtranquilao/ServidorPublicoApiApplication.java
+++ ServidorPublicoAPI/src/main/java/br/gov/jovemtranquilao/ServidorPublicoApiApplication.java
@@ -2,12 +2,26 @@ package br.gov.jovemtranquilao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ServidorPublicoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServidorPublicoApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
